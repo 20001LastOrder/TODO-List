ae4cfdcd319ce943098b625d504798cb9729e7f2
h4xiang <h4xiang@uwaterloo.ca>
2018-02-03 16:32:30 -0500

    updated controller

--- ToDoList/src/main/java/Controller/ViewController.java
+++ ToDoList/src/main/java/Controller/ViewController.java
@@ -10,10 +10,31 @@ import java.util.List;
 public class ViewController {
 	String taskName; // This should be linked to some text input in View
 	int taskListCounter = 1;
+	private todo.model.ToDo myToDoList = new todo.model.ToDo();
+	private todo.model.TaskList task_list;
 	
-	private todo.model.TaskList task_list = new todo.model.TaskList("List " + taskListCounter);
-	
-	private void addCategoryPerformed(java.awt.event.ActionEvent evt) {
-		
+	/*
+	 * Initialize a new task_list
+	 * Must be called first before adding catagories
+	 */
+	private void task_list_init(String name) {
+		if (null == name) {
+		task_list = new todo.model.TaskList("List " + taskListCounter, myToDoList);
+		} else {
+			task_list =  = new todo.model.TaskList("List " + name, myToDoList);
+		}
 	}
+	
+	/*
+	 * Add new catagory to the existing task_list
+	 */
+	private void addCategoryPerformed(java.awt.event.ActionEvent evt) {
+		if (null == task_list) {
+			System.err.println("Error: Catagory added when no task_list initialized");
+		} else {
+			todo.model.Catagory newCatagory = new todo.model.Catagory (taskName, task_list);
+		}
+	}
+	
+	private void 
 }
